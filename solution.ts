f62f09999b1a13b2597a553c61b475e32d4194fd
Mokarama <mokaramaaktershanta@gmail.com>
2025-11-17 23:39:44 +0600

    Add Problem-4 solution with rating filter function

--- solution.ts
+++ solution.ts
@@ -1,7 +1,7 @@
 // Problem-01
 
-/*
 
+/*
 function formatValue(input:string | number | boolean){
     if(typeof input === 'string'){
         const result=input.toUpperCase();
@@ -39,23 +39,23 @@ function getLength(input:string | number[]):number{
 
 console.log(getLength('typescript'));
 console.log(getLength([10, 20, 30, 40]));
-*/
 
+*/
 
 
 //problem-3
 
 
-
+/*
 class Person{
         name:string;
         age:number;
 
-    constructor(name:string, age:number):string{
+    constructor(name:string, age:number){
             this.name=name;
             this.age=age;
     }
-    getDetails():string{
+    getDetails(){
         console.log(`'Name:${this.name}, Age: ${this.age}'`);
     }
 }
@@ -66,10 +66,27 @@ console.log(person1.getDetails());
 
 const person2 = new Person('Alice', 25);
 console.log(person2.getDetails());
+*/
+
+//problem-4
+
+type Item={
+    title:string;
+    rating:number;
+};
+
+function filterByRating(items: Item[]):Item[]{
+    const rating= items.filter(item => item.rating >=4);
+    return rating;
+}
+
+const books:Item[] = [
+  { title: 'Book A', rating: 4.5 },
+  { title: 'Book B', rating: 3.2 },
+  { title: 'Book C', rating: 5.0 },
+];
 
 
 
-
-
-
+console.log(filterByRating(books));
 
