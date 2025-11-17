8d4337aa08c5c4118d9c303dbde5fb4c925a79fd
Mokarama <mokaramaaktershanta@gmail.com>
2025-11-17 21:56:56 +0600

    Solve Problem 3 using class-based implementation

--- solution.ts
+++ solution.ts
@@ -26,7 +26,7 @@ console.log(formatValue(true));
 
 //problem-2
 
-
+/*
 function getLength(input:string | number[]):number{
    if(typeof input === 'string'){
     return input.length;
@@ -39,6 +39,34 @@ function getLength(input:string | number[]):number{
 
 console.log(getLength('typescript'));
 console.log(getLength([10, 20, 30, 40]));
+*/
+
+
+
+//problem-3
+
+
+
+class Person{
+        name:string;
+        age:number;
+
+    constructor(name:string, age:number):string{
+            this.name=name;
+            this.age=age;
+    }
+    getDetails():string{
+        console.log(`'Name:${this.name}, Age: ${this.age}'`);
+    }
+}
+
+
+const person1 = new Person('John Doe', 30);
+console.log(person1.getDetails());
+
+const person2 = new Person('Alice', 25);
+console.log(person2.getDetails());
+
 
 
 
